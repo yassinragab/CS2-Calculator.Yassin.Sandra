d1c96a8792ea867f91089869c42d4d662b315f66
Yassin <yassinragab@aucegypt.edu>
2025-04-03 10:36:53 +0200

    Source file for calculator created

---
+++ calculator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "calculator.h"
+using namespace std;
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+double divide(double a, double b) {
+    if (b == 0) {
+        std::cout << "Error: Division by zero!\n";
+        return 0;
+    }
+    return a / b;
+}
+
+long long factorial(int n) {
+    if (n < 0) return -1;
+    long long fact = 1;
+    for (int i = 2; i <= n; ++i) fact *= i;
+    return fact;
+}
+
+int gcd(int a, int b) {
+    while (b) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int randomNumber(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+
