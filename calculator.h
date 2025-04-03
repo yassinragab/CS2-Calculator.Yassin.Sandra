c9c23f1b8f639e93a1da70f9c9ccf545aa61cb29
Yassin <yassinragab@aucegypt.edu>
2025-04-03 10:48:50 +0200

    Adding CMake file to repo

---
+++ calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+double divide(double a, double b);
+long long factorial(int n);
+int gcd(int a, int b);
+int lcm(int a, int b);
+int randomNumber(int min, int max);
+
+#endif
