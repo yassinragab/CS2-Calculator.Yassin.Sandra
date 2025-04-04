d229a6e840628dfd60f7a96149e2f782051cb337
Sandra walid <sandrawalid@aucegypt.edu>
2025-04-04 09:38:10 +0200

    added a test for division by 0

--- test.cpp
+++ test.cpp
@@ -10,7 +10,7 @@ int main(){
     cout<< "test Subtraction 10-4=" << subtract(10, 4) <<"\n";
     cout<< "test Multiplication 6*7=:" << multiply(6, 7) <<"\n";
     cout<< "test Division 15/3=" << divide(15, 3) <<"\n";
-    cout<< "test Division by 0: 15/0=" << divide(15, 0) <<"\n";
+    cout<< "test Division by 0: 15/0=" << divide(15, 0) <<"\n";//test for division by 0
     cout<< "test Factorial: " << factorial(5) <<"\n";
     cout << "test GCD: " << gcd(48, 18) <<"\n";
     cout << "test LCM: " << lcm(12, 15) <<"\n";
